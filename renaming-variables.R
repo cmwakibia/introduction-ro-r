a5b5bf132da9c50b810622b76573a7f8342db630
cmwk <cmwwakibia@gmail.com>
2024-09-13 19:06:07 +0300

    added a file

---
+++ renaming-variables.R
@@ -0,0 +1,12 @@
+library(dplyr)
+
+# Sample data frame
+df <- data.frame(X1 = 1:5, X2 = 6:10, X3 = 11:15)
+
+# Select columns where the column is numeric and rename them
+renamed_df <- df %>%
+  select(where(is.numeric)) %>%
+  rename_with(~paste0("Column_", .), everything())
+
+# View the renamed data frame
+print(renamed_df)
